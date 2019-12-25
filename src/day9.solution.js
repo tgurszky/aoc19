d647d93636ba010e1dfbe582655a416d7671d404
Gurszky Tamás <gurszky.tamas@danubiusinfo.hu>
2019-12-25 10:49:23 +0100

    day 9 part 2 solution

--- src/day9.solution.js
+++ src/day9.solution.js
@@ -2,6 +2,8 @@ import { calculateProgram } from "./computer";
 import { day9Program } from "../inputs/day9.input";
 
 const output = [];
-calculateProgram(day9Program, 1, output);
+calculateProgram(day9Program.slice(), 1, output);
+calculateProgram(day9Program.slice(), 2, output);
 
-console.log(`part 1 result is ${JSON.stringify(output)}`);
+console.log(`part 1 result is ${output[0]}`);
+console.log(`part 2 result is ${output[1]}`);
